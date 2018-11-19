60c85405219a0a4e9f8403a386e1b8ed24b4b183
David Haynes <dahaynes@umn.edu>
2018-11-19 00:31:53 -0600

    Downloading census block files

--- Loading.py
+++ Loading.py
@@ -27,12 +27,15 @@ def LoadShapeFile(theShpPath, db, theCollection, SRID=4326):
     """
 
     """    
-    with fiona.open(myShpPath, 'r', crs=SRID) as theShp:
+    import csv
+    with fiona.open(myShpPath, 'r', crs=SRID) as theShp, open(r"c:\work\mongoErrors2.csv", 'w', newline='\n') as fout:
+        
         if theCollection in mongoDB.list_collection_names():
             print("Found Existing collection with same name %s \n Dropping old collection" % (theCollection))
             mongoDB.drop_collection(theCollection)
         
-        
+        theCSV = csv.writer(fout)
+        theCSV.writerow(['feature', 'id', 'type', 'issues'])
         mongoCollection  = db[theCollection]
         
         for f, feature in enumerate(theShp):
@@ -41,38 +44,39 @@ def LoadShapeFile(theShpPath, db, theCollection, SRID=4326):
             if featureType == 'Polygon':
                 featureCoordinates = feature['geometry']['coordinates']
                 mongoDBCoordinates = [list(p) for polys in featureCoordinates for p in polys]
-        
-                #fakeCoordinates = [[ [-96.50, 49.14 ], [-90.77, 48.46], [-91.00, 45.98], [-97.37, 46.00], [-96.50, 49.14 ] ]]
-    
-                
-                mongoCollection.insert_one({'geom': {'type': featureType, 'coordinates': [mongoDBCoordinates] }} )
-                print("Loaded feature %s of %s" % (f+1, len(theShp)))
-                #break
-                #return(mongoDBCoordinates, featureCoordinates)
-                #break
+                mongoCollection.insert_one({'geom': {'type': featureType, 'coordinates': [mongoDBCoordinates] }, 'name': feature['properties']['NAME'] })
+                #print("Loaded feature %s of %s" % (f+1, len(theShp)))
+                theCSV.writerow([f, feature['properties']['ID'], featureType, 'None' ])
             
             else:
                 multiPolygon = []
                 for polygon in feature['geometry']['coordinates']:
                     
                     mongoDBCoordinates = [list(p) for polys in polygon for p in polys]
+                    if mongoDBCoordinates[0][0] == mongoDBCoordinates[-1][0] and mongoDBCoordinates[0][1] == mongoDBCoordinates[-1][1]: 
+                        issue = "closed polygon"
+                    else:
+                        issue = "open polygon"
+                        mongoDBCoordinates.append(mongoDBCoordinates[0])
+                    theCSV.writerow([f, feature['properties']['ID'], featureType, issue ])
+                    mongoDBCoordinates = tuple(mongoDBCoordinates)
                     multipart = [mongoDBCoordinates]
                     multiPolygon.append(multipart)
-                
-                mongoCollection.insert_one({'geom': {'type': featureType, 'coordinates': multiPolygon }} )
-                print("Loaded feature %s of %s" % (f+1, len(theShp)))
 
-                #return(feature, multiPolygon, mongoDBCoordinates)
+                
+                mongoCollection.insert_one({'geom': {'type': featureType, 'coordinates': multiPolygon }, 'name': feature['properties']['NAME'] })
+                
+            
+            print("Loaded feature %s of %s" % (f+1, len(theShp)))
                 
         #mongoCollection.create_index([('coordinates', pymongo.GEO2D)])
         mongoCollection.create_index([('geom', pymongo.GEOSPHERE)])
         #mongoCollection.create_index([('coordinates', pymongo.GEOSPHERE)])
             
         
-    
         
             
-myShpPath = r"C:\scidb\us_states.shp" #r"C:\scidb\mn_county_boundaries.shp" #r"C:\scidb\shapefiles\4326\counties.shp"
+myShpPath = r"C:\scidb\us_counties.shp" #r"C:\scidb\mn_county_boundaries.shp" #r"C:\scidb\shapefiles\4326\counties.shp"
 directory, shapeFileName = os.path.split(myShpPath)
 collectionName = shapeFileName.split('.')[0]
 
