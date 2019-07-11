c940a08abbb7c2fc1ed4fb56d06912abedf88782
David Haynes <dahaynes@umn.edu>
2019-07-11 00:12:57 -0500

    july clean up

---
+++ js/lookup.js
@@ -0,0 +1,33 @@
+db.two_states.aggregate(
+    [
+        { $lookup:
+            {   from: "points",
+                localField: "HASH_2",
+                foreignField: "HASH_2",
+                as: "points"
+            }  
+        },
+        { 
+            $unwind: "$points"
+        }
+//         ,{
+//         $group: {
+//                     _id: "HASH_2", count: { $sum: 1 }
+//                 }
+//             }
+//         ,{
+//             "$match": {
+//                 "geom": {
+//                     $geoIntersects: {
+//                         $geometry: {
+//                                 tojsonObject("$points.geom")
+// //                             "type": "Point",
+// //                             "coordinates": [parseFloat("$points.geom.coordinates[0]"),parseFloat("$points.geom.coordinates[1]")] //[-107.862889172811, 38.8613591309177]
+//                             }
+//                         }
+//                     }
+//                 }
+//         }
+    
+    ]
+)
--- vector_cleaning/mongo_loader.py
+++ vector_cleaning/mongo_loader.py
@@ -6,16 +6,15 @@ This script loads shapefiles into MongoDB
 
 
 
-#from osgeo import ogr
-#import geopandas as gpd
-#import psycopg2, shapely
-import fiona, os
+# from osgeo import ogr
+# import geopandas as gpd
+# import psycopg2, shapely
+import fiona, os, csv
 from shapely.geometry import *
+from shapely.wkt import dumps, loads
 from pymongo import MongoClient, GEOSPHERE, HASHED
 from pymongo.errors import WriteError
 import timeit
-
-
 
 
 
@@ -43,19 +42,21 @@ def CreateMongoCollection(theCon, collectionName ):
     
 def CreateSpatialIndex(theCollection):
     """
-    
+    This function create the Spatial Index  
     """
     
     theCollection.create_index([('geom', GEOSPHERE)])
 
 def CreatGeoHashedIndex(theCollection, hashKey):
     """
+    Creates a hashed index on a distributed dataset
     db.random10m_points_hashed.ensureIndex({HASH_2: "hashed"})
     """
     
     theCollection.create_index([(hashKey, HASHED)])
 
-def CreateMonogPoint(theCoordinates):
+
+def CreateMongoPoint(theCoordinates):
     """
     
     { type: "Point", coordinates: [ 40, 5 ] }
@@ -63,8 +64,17 @@ def CreateMonogPoint(theCoordinates):
     """
     return(list(theCoordinates))
 
+def CreateMongoLine(coordinateList):
+    """
+    { type: "LineString", coordinates: [ [ 40, 5 ], [ 41, 6 ] ] }
+    """
+
+    return( [list(c) for c in coordinateList ])
+
+
 def CreateMongoPolygon(theFeature, ):
     """
+    This is how Mongo wants a polygon
     {
      type : "Polygon",
      coordinates : [
@@ -74,7 +84,6 @@ def CreateMongoPolygon(theFeature, ):
      }
     """
     
-
     arrayCoordinates = []
     dataset = [ list(pointpair) for pointpair in theFeature.exterior.coords ]
         #ringCoordinates = [list(pointPair) for ring in theFeaturePoints for pointPair in ring]            
@@ -101,6 +110,44 @@ def progress(count, total, status=''):
     print('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
 
 
+def ReadCSV(inFilePath, geomField="geom_text"):
+    """
+    
+    """
+    with open(r'c:\scidb\counties.csv', 'r') as fin:
+        csvIn = csv.DictReader(fin, delimiter=";")
+        for rec in csvIn:
+            rec["geom"] = loads(rec['geom_text'])
+            
+            
+    return rec.type
+        
+def ReadShapefile(inFilePath):
+    """
+    
+    """
+    with fiona.open(inFilePath, 'r', crs=4326) as theShp:
+        pass
+         
+        
+def ShardCollection(mongoCon, databaseName, collectionName, shardkey, ):
+    """
+
+    """
+    #print("Creating Hashed Index on %s" % (shardkey))
+    #CreatGeoHashedIndex(mongoCollection, shardkey)
+    databaseMongoCollectionName = "%s.%s" % ("research", collectionName)
+    print("Sharding collection by key: %s" % (shardkey))
+    #mongoDB.admin.c
+    #This command is still failing
+    adminDB = mongoCon['admin']
+    adminDB.command('enableSharding', databaseName)
+    #print(adminDB.command('listCommands'))
+    print("""{%s: "%s"}, key: {%s: "hashed"} """ % ("shardCollection", databaseMongoCollectionName, shardkey ))
+    adminDB.command({"shardCollection": databaseMongoCollectionName, "key":{shardkey: "hashed"}})
+    #usemongoDB.admin.command('shardCollection', databaseMongoCollectionName, key={shardkey: "hashed"})
+    del adminDB, mongoDB    
+
 def LoadShapefile(inFile, collectionName, mongoPort, mongoDatabase="research", shardkey=None):
     """
     Function for loading a shapefile
@@ -108,38 +155,28 @@ def LoadShapefile(inFile, collectionName, mongoPort, mongoDatabase="research", s
     mongoCon = CreateMongoConnection(host='localhost', port=mongoPort)
     databaseName = str(mongoDatabase)
     mongoDB = mongoCon[databaseName]
+    #Not sure if this is beigng used
     shapeDir, shapeFileName = os.path.split(inFile)
     
     CreateMongoCollection(mongoDB, collectionName )
     mongoCollection  = mongoDB[collectionName]
     
     if shardkey:
-        #print("Creating Hashed Index on %s" % (shardkey))
-        #CreatGeoHashedIndex(mongoCollection, shardkey)
-        databaseMongoCollectionName = "%s.%s" % ("research", collectionName)
-        print("Sharding collection by key: %s" % (shardkey))
-        #mongoDB.admin.c
-        #This command is still failing
-        adminDB = mongoCon['admin']
-        adminDB.command('enableSharding', databaseName)
-        #print(adminDB.command('listCommands'))
-        print("""{%s: "%s"}, key: {%s: "hashed"} """ % ("shardCollection", databaseMongoCollectionName, shardkey ))
-        adminDB.command({"shardCollection": databaseMongoCollectionName, "key":{shardkey: "hashed"}})
-        #usemongoDB.admin.command('shardCollection', databaseMongoCollectionName, key={shardkey: "hashed"})
-        del adminDB, mongoDB
+        ShardCollection(mongoCon, databaseName, collectionName, shardkey, )
+
 
     with fiona.open(fileIn, 'r', crs=4326) as theShp:
          
         mongoDB = mongoCon['research'] 
         mongoCollection  = mongoDB[collectionName]
-        print(databaseName, mongoDatabase, mongoDB.list_collection_names())
+        # print(databaseName, mongoDatabase, mongoDB.list_collection_names())
         badGeoms = []
     
         for f, feature in enumerate(theShp):
             featureType = feature['geometry']['type']
             theFeaturePoints = feature['geometry']['coordinates']
     
-            if featureType ==  "Polygon":            
+            if featureType.upper() ==  "Polygon".upper():            
                 if len(feature['geometry']['coordinates']) == 1:
                     #Single ring polygons
                     theFeature = Polygon(theFeaturePoints[0])
@@ -152,8 +189,7 @@ def LoadShapefile(inFile, collectionName, mongoPort, mongoDatabase="research", s
                     mongoCoordinates = CreateMongoPolygon(theFeature)
                     insertData = True
             
-            
-            elif featureType == 'MultiPolygon':
+            elif featureType.upper() == "MultiPolygon".upper():
                 #This isn't the best way to make the shapely multipolygon
                 listofPolygons  = [ Polygon(poly[0]) for poly in theFeaturePoints]
                 theFeature = MultiPolygon(listofPolygons)
@@ -164,13 +200,29 @@ def LoadShapefile(inFile, collectionName, mongoPort, mongoDatabase="research", s
                         polygonCoordinates = CreateMongoPolygon(aPolygon)
                         mongoCoordinates.append(polygonCoordinates)
                         insertData = True
-                        
-            elif featureType == "Point":
+            
+            elif featureType == "LineString".upper():
+                # { type: "LineString", coordinates: [ [ 40, 5 ], [ 41, 6 ] ] }
+                mongoCoordinates = CreateMongoLine(theFeaturePoints)
+
+            elif featureType == "MultiLineString".upper():
+                    #  {
+                    #   type: "MultiLineString",
+                    #   coordinates: [
+                    #      [ [ -73.96943, 40.78519 ], [ -73.96082, 40.78095 ] ],
+                    #      [ [ -73.96415, 40.79229 ], [ -73.95544, 40.78854 ] ],
+                    #      [ [ -73.97162, 40.78205 ], [ -73.96374, 40.77715 ] ],
+                    #      [ [ -73.97880, 40.77247 ], [ -73.97036, 40.76811 ] ]
+                    #   ]
+                    # }
+                pass
+
+            elif featureType == "Point".upper():
                 #{ type: "Point", coordinates: [ 40, 5 ] }
-                mongoCoordinates = CreateMonogPoint(theFeaturePoints)
+                mongoCoordinates = CreateMongoPoint(theFeaturePoints)
                 insertData = True
                 
-            elif featureType == "MultiPoint":
+            elif featureType == "MultiPoint".upper():
                 #{
                 #  type: "MultiPoint",
                 #  coordinates: [
