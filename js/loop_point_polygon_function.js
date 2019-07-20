ffa46861613215ac923feccb529ba9a8d043066c
David Haynes <dahaynes@umn.edu>
2019-07-19 22:07:53 -0500

    Update loop_point_polygon_function.js

--- js/loop_point_polygon_function.js
+++ js/loop_point_polygon_function.js
@@ -5,25 +5,29 @@ var startTime = new Date().getTime();
 while (polyCursor.hasNext()) {
     var poly = polyCursor.next()
     var results = db.getCollection(pointCollection).aggregate(
-	[
-		{
-		    "$match": {
-		        "geom": {
-		            $geoIntersects: {
-		                $geometry: poly.geom
-		            }
-		        }
-		    }
-		},
-		{
-		    "$count": "num_points"
-		}
-	]);
-	var points = results._batch[0].num_points;
-	print(poly.NAME, points);
+    [
+        {
+            "$match": {
+                "geom": {
+                    $geoIntersects: {
+                        $geometry: poly.geom
+                    }
+                }
+            }
+        },
+        {
+            "$count": "num_points"
+        }
+    ]);
+    
+    if (results._batch.length > 0){
+        var points = results._batch[0].num_points;
+        print(poly.place_name, points);       
+    } else {
+        print(poly.place_name, 0);        
+    }
+        
 }
 var stopTime = new Date().getTime();
 print("Elapsed Time: ", stopTime-startTime)
 }
-
-pointPolygonCount("states_hashed", "randompoints_10m_hashed")
